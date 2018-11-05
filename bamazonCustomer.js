28edc872a5c2ac5380986a5b3872e8355df929d0
danielpv <danielpvk@gmail.com>
2018-11-04 20:40:33 -0600

    customer ready

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 //var genre = process.argv[2]+" "+process.argv[3];
 //console.log(genre);
 
@@ -21,13 +22,139 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
   afterConnection();
 });
+var databaseParse = {
+  productNames:[],
+  itemsIDs:[],
+  department_names:[],
+  prices:[],
+  stock_quantities:[]
+};
+var productToBuy="";
+var productIndex=null;
+var productQuantity=0;
+var quantityToBuy=0;
 
-var example="memories";
+function resParse(res){
+  for (var i=0;i<res.length;i++){
+      databaseParse.itemsIDs.push(res[i].item_id);
+      databaseParse.productNames.push(res[i].product_name);
+      databaseParse.department_names.push(res[i].department_name);
+      databaseParse.prices.push(res[i].price);
+      databaseParse.stock_quantities.push(res[i].stock_quantity);
+  }
+ // console.log(res);
+  //console.log("ids",databaseParse.itemsIDs);
+  //console.log("products",databaseParse.productNames);
+}
+function buy(){
 
-function afterConnection() {
-  connection.query("SELECT * FROM products WHERE department_name=?",(example), function(err, res) {
+  var restante=productQuantity-quantityToBuy;
+  var sql="UPDATE products SET stock_quantity="+restante+" WHERE item_id="+productIndex;
+  connection.query(sql, function(err, res) {
+       if (err) throw err;
+       if (res.affectedRows==1){
+        console.log("Purchase completed");
+       }
+       else{
+         console.log("Something go wrong, purchase not possible.");
+    
+       }
+       
+    });
+}
+
+function howMany()
+{
+  var mssg="How many "+productToBuy+" do you want to buy ? ("+productQuantity+" available) :";
+  inquirer.prompt(
+    {
+      type: "input",
+      name: "qty",
+      message: mssg
+    }
+  ).then(function(action){
+      if(action.qty<=productQuantity){
+        quantityToBuy=action.qty;
+        buy();
+      }
+      else{
+        console.log("Not enough products available. ");
+      }
+  });
+
+}
+
+
+function quantityProduct(){
+  connection.query("SELECT stock_quantity FROM products WHERE item_id=?",(productIndex), function(err, res) {
+    //  connection.end();
+      if (err) throw err;
+  //    console.log(res);
+      productQuantity=res[0].stock_quantity;
+//      console.log("Quantity available :",productQuantity);
+      howMany();
+    });
+
+}
+
+function indexProduct(product){
+  connection.query("SELECT item_id FROM products WHERE product_name=?",(product), function(err, res) {
+  //  connection.end();
     if (err) throw err;
-    console.log(res);
-    connection.end();
+    //console.log(res);
+    productIndex=res[0].item_id;
+    //console.log(productIndex);
+    quantityProduct();
   });
 }
+
+
+function whatToBuy(products)
+{
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "product",
+      message: "Which product do you want to buy?",
+      choices: products
+      
+    }
+  
+  ]).then(function(action) {
+    // Choose item to buy...
+   // console.log("product",action.product);
+   // console.log("action",action);
+    if (action.product){
+      //how many items
+      productToBuy=action.product;
+      indexProduct(productToBuy);
+      
+      
+    }
+    else {
+        console.log("Option not available, try 1 to 10");
+    }
+  });
+}  
+
+
+
+
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+     if (err) throw err;
+    //console.log("ItemID     Product Name                               Quantity Av.   Department Name");
+   // console.log(res);
+    //console.table(["1","3","33"]);
+   resParse(res);
+   whatToBuy(databaseParse.productNames);
+   
+   
+   
+   //connection.end();
+   
+  });
+}
+
+
